76d4368d4033252577565285d0fc5cf9fbfa6e47
Prikus-01 <9riyanshu.k@gmail.com>
2025-02-19 16:15:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: 'https://quiz-wizard-frontend.vercel.app',
+    credentials: true
+}))
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
