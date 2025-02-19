f08e89cdf4a663ddbc822737fd06e780d490af12
Prikus-01 <9riyanshu.k@gmail.com>
2025-02-19 16:44:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 
 const app = express();
 app.use(cors({
-    origin: 'https://quiz-wizard-backend.vercel.app',
+    origin: 'https://quiz-wizard-frontend.vercel.app',
     credentials: true
 }))
 // app.use(cors({ origin: "*" }));
