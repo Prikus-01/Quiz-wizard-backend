8086614adba930fb21db5e420d08252d2b448ced
Prikus-01 <9riyanshu.k@gmail.com>
2025-02-19 16:43:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-// app.use(cors({
-//     origin: 'https://nancy-frontend-app.vercel.app',
-//     credentials: true
-// }))
-app.use(cors({ origin: "*" }));
+app.use(cors({
+    origin: 'https://quiz-wizard-backend.vercel.app',
+    credentials: true
+}))
+// app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
