fdae99873b4ef0d37fbd8d3b4f2fe97825cb4a14
Garry <priyanshu.k22@iiits.in>
2025-02-21 14:57:08 +0530

    localhost

--- server.js
+++ server.js
@@ -4,16 +4,25 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-app.use(cors({
-    origin: 'https://quiz-wizard-frontend.vercel.app',
-    credentials: true
-}))
+app.use(
+  cors({
+    origin: [
+      "https://quiz-wizard-frontend.vercel.app",
+      "http://localhost:3000",
+    ],
+    credentials: true,
+  })
+);
 // app.use(cors({ origin: "*" }));
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.log(err));
+  .catch((err) => console.log(err));
 
 const quizRoutes = require("./routes/quizRouter");
 app.use("/api/quiz", quizRoutes);
